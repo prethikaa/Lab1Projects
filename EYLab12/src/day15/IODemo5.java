4cec28611dbabedf006eae2fb3a6d30d2fcda745
unknown <prethikaam@gmail.com>
2021-03-14 13:01:46 +0530

    commit changes

---
+++ EYLab12/src/day15/IODemo5.java
@@ -0,0 +1,12 @@
+package day15;
+
+import java.io.ByteArrayInputStream;
+import java.io.StringBufferInputStream;
+
+public class IODemo5 {
+	public static void main(String[] args) {
+		ByteArrayInputStream bis=new ByteArrayInputStream("helloworld".getBytes());
+		StringBufferInputStream sbis=new StringBufferInputStream("hello hai world");
+		
+	}
+}
